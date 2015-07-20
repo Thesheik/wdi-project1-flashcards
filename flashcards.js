040e9d9e51396de227285b6054efe95e4a58a96a
Cheikh Ndiongue <Cmndiongue@yahoo.com>
2015-07-20 09:11:37 -0400

    updated project

--- flashcards.js
+++ flashcards.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-   console.log( "ready!" )
+
+  console.log( "ready!" )
+
+    $(".back").hide();
+
+    $(".front").click(function(e){
+        $(this).next().show();
+        $(this).hide();
+     });
+
+    $(".back").click(function(){
+        $(this).prev().show();
+        $(this).hide();
+    });
+
 });
